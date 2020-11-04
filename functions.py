14ce58a4705a9b4b772f9ea6764b86cac9992330
szilard <kalosi.szilard@gmail.com>
2020-11-04 09:08:51 +0100

    dont show spinner

--- functions.py
+++ functions.py
@@ -7,7 +7,7 @@ import geopandas as gpd
 from conts import berlin_pop
 
 
-@st.cache
+@st.cache(show_spinner=False)
 def get_infection_data():
     url = "https://www.berlin.de/lageso/gesundheit/infektionsepidemiologie-infektionsschutz/corona/tabelle-bezirke-gesamtuebersicht/index.php/index/index.json?q="
     with urllib.request.urlopen(url) as response:
@@ -22,7 +22,7 @@ def get_infection_data():
     return df.iloc[:, 2:].sum() / berlin_pop, df["Datum"].agg(["min", "max"])
 
 
-@st.cache
+@st.cache(show_spinner=False)
 def get_berlin():
     berlin = gpd.read_file("berlin_bezirke.geojson")
     centroids = berlin["geometry"].to_crs(epsg=3035).centroid.to_crs(epsg=4326)
