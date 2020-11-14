315d1ae9853087c2a8bf275c7826a91c3427fca1
kalosisz <kalosi.szilard@gmail.com>
2020-11-14 17:23:11 +0100

    get remote csv instead of json directly in pandas

--- functions.py
+++ functions.py
@@ -1,20 +1,17 @@
-import urllib
-import json
 import streamlit as st
 import pandas as pd
 import geopandas as gpd
+
+from retrying import retry
 
 from constants import berlin_pop
 
 
 @st.cache(show_spinner=False, max_entries=1, ttl=3600)
+@retry(wait_fixed=1000, stop_max_attempt_number=10)
 def get_infection_data():
-    url = "https://www.berlin.de/lageso/gesundheit/infektionsepidemiologie-infektionsschutz/corona/tabelle-bezirke-gesamtuebersicht/index.php/index/index.json"
-    with urllib.request.urlopen(url) as response:
-        df = pd.DataFrame(json.loads(response.read())["index"][-7:])
-
-    for col in df.columns[2:]:
-        df[col] = df[col].astype(int)
+    csv_loc = "https://www.berlin.de/lageso/gesundheit/infektionsepidemiologie-infektionsschutz/corona/tabelle-bezirke-gesamtuebersicht/index.php/index/all.csv"
+    df = pd.read_csv(csv_loc, sep=";").iloc[-7:]
 
     df.columns = df.columns.str.replace("_", "-").str.title().str.replace(
         "oe", "ö")
